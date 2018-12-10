6bfe222d1a9e1850d72f59d20722596839247296
danielid258 <danielid258@163.com>
2018-12-10 10:51:40 +0800

    distribution lock

--- src/main/java/com/daniel/interview/aspect/LogAspect.java
+++ src/main/java/com/daniel/interview/aspect/LogAspect.java
@@ -19,6 +19,12 @@ public class LogAspect {
     @Pointcut("execution(public * com.daniel.interview.controller.*.insert*(..))")
     public void logPointcut(){}
 
+    /**
+     * ProceedingJoinPoint和JoinPoint的区别
+     * ProceedingJoinPoint继承了JoinPoint 并在JoinPoint的基础上暴露出proceed方法 proceed用于显式调用目标方法
+     * 暴露出proceed就能支持 aop:around 这种切面(环绕通知=前置通知+目标方法执行+后置通知)
+     * 如果不调用proceed则目标方法不会被执行 前置通知和后置通知都默认调用目标方法
+     */
     @Around("logPointcut()")
     public Object saveLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info(String.format("============== before target: %s", joinPoint.getSignature().getName()));
